ca50e24ce6cd40a5729eb2a70cf7a2be58592958
Mike Lovelace <mike@mlovelace.net>
2020-11-10 02:25:09 -0500

    task 1c and 1d

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ Do the following:
    HINT: look up the Number method
 */
 
-
-
+const year = "1999";
+console.log(Number(year))
 
 /*
 Task 1d - Multiply
@@ -69,10 +69,10 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-    /*add your code here*/
+function multiply(a, b){
+    return a * b
   }
-
+console.log(multiply(2, 4))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
